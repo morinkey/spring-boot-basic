d2808fa91092db1ee5c404c34600eb8d0c18787d
morinkey <info@javatechnology.net>
2016-07-06 00:09:06 +0900

    first commmit

---
+++ src/main/java/jp/co/example/SampleController.java
@@ -0,0 +1,13 @@
+package jp.co.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello!";
+    }
+}
